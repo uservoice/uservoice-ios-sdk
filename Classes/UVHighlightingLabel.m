908168a3dd776b476365125c2d868b528dfbc149
Austin Taylor <austin.taylor@gmail.com>
2012-11-30 17:27:54 -0500

    Rewrite rectForLetterAtIndex because it didn't actually support word wrapping correctly

--- Classes/UVHighlightingLabel.m
+++ Classes/UVHighlightingLabel.m
@@ -13,148 +13,77 @@
 
 @synthesize pattern;
 
-// https://gist.github.com/1278483
 - (CGRect)rectForLetterAtIndex:(NSUInteger)index {
-    NSAssert(self.lineBreakMode != UILineBreakModeClip, @"UILabel.lineBreakMode cannot be UILineBreakModeClip to calculate the rect of a character. You might think that it's possible, seeing as UILineBreakModeWordWrap is supported, and they are almost the same. But the semantics are weird. Sorry.");
-    NSAssert(self.lineBreakMode != UILineBreakModeHeadTruncation, @"UILabel.lineBreakMode cannot be UILineBreakModeHeadTruncation to calculate the rect of a character. We can't have everything you know.");
-    NSAssert(self.lineBreakMode != UILineBreakModeMiddleTruncation, @"UILabel.lineBreakMode cannot be UILineBreakModeMiddleTruncation to calculate the rect of a character. We can't have everything you know.");
-    /* NSAssert(self.lineBreakMode != UILineBreakModeTailTruncation, @"UILabel.lineBreakMode cannot be UILineBreakModeTailTruncation to calculate the rect of a character. We can't have everything you know."); */
- 
-    // Check if label is empty. Should add so it also checks for strings containing only spaces
-    if ( [self.text length] == 0 )
-    {
-        return self.bounds;
-    }
-    
-    // Algorithm goes like this:
-    //    1. Determine which line the letter is on
-    //    2. Get the x-position on the line by: width of string up to letter
-    //    3. Apply UITextAlignment to the x-position
-    //    4. Add y position based on height of letters * line number
-    //    Et víolà!
-    
+    if (index > [self.text length] - 1)
+        return CGRectZero;
+
+    CGFloat frameWidth = self.frame.size.width - 6;
     NSString *letter = [self.text substringWithRange:NSMakeRange(index, 1)];
-    
-    // Determine which line the letter is on and the string on that line
     CGSize letterSize = [letter sizeWithFont:self.font];
     
-    int lineNo = 0;
-    int linesDisplayed = 1;
-    
-    // Get the substring with the line on it
-    NSUInteger lineStartsOn = 0;
-    NSUInteger currentLineLength = 1;
-    
-    // Temporary variables
-    NSUInteger currentLineStartsOn = 0;
-    NSUInteger currentCurrentLineLength = 1;
-    
-    float currentWidth;
-    
-    // TODO: Add support for UILineBreakModeWordWrap, UILineBreakModeCharacterWrap to complete implementation
-    
-    // Get the line number of the current letter
-    // Get the contents of that line
-    // Get the total number of lines (which means that no matter what we loop through the entire thing)
-    
-    BOOL isDoneWithLine = NO;
-    
-    NSUInteger i = 0, len = [self.text length];
-    
-    // The loop is different depending on the lineBreakMode. If it is UILineBreakModeCharacterWrap it is easy
-    // just check for every single character. For UILineBreakModeWordWrap it is a bit more tedious. We have
-    // to think in terms of words. We have to find each word and check it. If it is longer than the frame width
-    // then we know we have a new word, and that lines index starts on the words beginning index. 
-    // Spaces prove to be even morse troublesome. Several spaces in a row at the end of a line won't result in
-    // any more width.
-    for ( ; i < len; i++ )
-    {
-        NSString *currentLine = [self.text substringWithRange:NSMakeRange(currentLineStartsOn, currentCurrentLineLength)];
-        
-        CGSize currentSize = [currentLine sizeWithFont:self.font constrainedToSize:CGSizeMake(self.frame.size.width, 1000) lineBreakMode:self.lineBreakMode];
-        currentWidth = currentSize.width;
-        
-        if ( currentSize.height > self.font.lineHeight )
-        {
-            // We have to go to a new line
-            linesDisplayed++;
-            
-            //NSLog(@"new line on: %d", i);
-            
-            // If i <= index that means we are on the correct letter's line
-            // store that
-            if ( i <= index )
-            {
-                lineStartsOn = i;
-                lineNo++;
-                currentLineLength = 1;
-            }
-            else
-                isDoneWithLine = YES;
-            
-            currentLineStartsOn = i;
-            currentCurrentLineLength = 1;
-            i--;
-        }
-        else
-        {
-            // Okay with the same line
-            
-            currentCurrentLineLength++;
-            
-            if ( ! isDoneWithLine )
-            {
-                currentLineLength++;
-            }
+    NSArray *lines = [self breakString];
+    int targetLineNumber = 0, targetColumnNumber = 0, elapsedChars = 0;
+    NSString *targetLine;
+    for (int i = 0; i < [lines count]; i++) {
+        NSString *line = [lines objectAtIndex:i];
+        if (index >= elapsedChars + [line length]) {
+            elapsedChars += [line length];
+            targetLineNumber++;
+        } else {
+            targetLine = line;
+            targetColumnNumber = index - elapsedChars;
+            break;
         }
     }
+
+    int linesThatFit = floor(self.frame.size.height / self.font.lineHeight);
+    int totalLines = self.numberOfLines == 0 ? [lines count] : MIN([lines count], self.numberOfLines);
+    int linesDisplayed = MIN(linesThatFit, totalLines);
+    CGFloat targetLineWidth = [targetLine sizeWithFont:self.font].width;
     
-    // Make sure we didn't overstep the bounds
-    while ( lineStartsOn + currentLineLength > len )
-        currentLineLength--;
+    CGFloat x = [[targetLine substringWithRange:NSMakeRange(0, targetColumnNumber)] sizeWithFont:self.font].width;
+    CGFloat y = self.frame.size.height/2 - (linesDisplayed*self.font.lineHeight)/2 + self.font.lineHeight*targetLineNumber;
     
-    // Check if linesDisplayed is faulty, if for example lines have been clipped
-    CGSize totalSize = [self.text sizeWithFont:self.font constrainedToSize:CGSizeMake(self.frame.size.width, 100000) lineBreakMode:self.lineBreakMode];
-    
-    if ( totalSize.height > self.frame.size.height )
-    {
-        // It has been clipped, calculate how many lines are actually shown
-        
-        linesDisplayed = 0;
-        float lineHeight = 0;
-        while ( lineHeight < self.frame.size.height )
-        {
-            lineHeight += self.font.lineHeight;
-            linesDisplayed++;
-        }
-        
-        linesDisplayed--;
-        
-        // Number of lines is not automatic, keep it within that range
-        if ( self.numberOfLines > 0 )
-            linesDisplayed = linesDisplayed > self.numberOfLines ? self.numberOfLines : linesDisplayed;
-    }
-    
-    // Length of the substring up and including this letter
-    NSUInteger currentLineSubstrLength = index - lineStartsOn + 1;
-    
-    currentWidth = [[self.text substringWithRange:NSMakeRange(lineStartsOn, currentLineLength)] sizeWithFont:self.font].width;
-    
-    NSString *lineSubstr = [self.text substringWithRange:NSMakeRange(lineStartsOn, currentLineSubstrLength)];
-    
-    float x = [lineSubstr sizeWithFont:self.font].width - [letter sizeWithFont:self.font].width;
-    float y = self.frame.size.height/2 - (linesDisplayed*self.font.lineHeight)/2 + self.font.lineHeight*lineNo;
-    
-    if ( self.textAlignment == UITextAlignmentCenter )
-    {
-        x = x + (self.frame.size.width-currentWidth)/2;
-    }
-    else if ( self.textAlignment == UITextAlignmentRight )
-    {
-        x = self.frame.size.width-(currentWidth-x);
-    }
+    if (self.textAlignment == UITextAlignmentCenter)
+        x = x + (frameWidth-targetLineWidth)/2;
+    else if (self.textAlignment == UITextAlignmentRight)
+        x = frameWidth-(targetLineWidth-x);
     
     return CGRectMake(x, y, letterSize.width, letterSize.height);
+}
+
+- (NSArray *)breakString {
+    NSMutableArray *lines = [NSMutableArray array];
+    CGFloat frameWidth = self.frame.size.width - 6;
+    int len = [self.text length];
+    int lineStartOffset = 0;
+    int lastBreakChar = -1;
+
+    for (int i=0; i < len; i++) {
+        int currentLineLength = i - lineStartOffset;
+        NSString *currentChar = [self.text substringWithRange:NSMakeRange(i, 1)];
+        NSString *currentLine = [self.text substringWithRange:NSMakeRange(lineStartOffset, currentLineLength)];
+        if ([currentChar isEqualToString:@" "] || [currentChar isEqualToString:@"-"])
+            lastBreakChar = i;
+        
+        CGSize currentSize = [currentLine sizeWithFont:self.font constrainedToSize:CGSizeMake(frameWidth, 1000) lineBreakMode:self.lineBreakMode];
+        
+        // TODO: Add support for hard breaks (\n)
+        if (currentSize.height > self.font.lineHeight) {
+            if (lastBreakChar == -1 || self.lineBreakMode == UILineBreakModeCharacterWrap) {
+                currentLine = [self.text substringWithRange:NSMakeRange(lineStartOffset, currentLineLength)];
+                lineStartOffset = i;
+                i--;
+            } else {
+                currentLine = [self.text substringWithRange:NSMakeRange(lineStartOffset, lastBreakChar - lineStartOffset + 1)];
+                i = lineStartOffset = lastBreakChar + 1;
+                lastBreakChar = -1;
+            }
+            [lines addObject:currentLine];
+        }
+    }
+    [lines addObject:[self.text substringWithRange:NSMakeRange(lineStartOffset, len - lineStartOffset)]];
+    return lines;
 }
 
 - (void)drawRect:(CGRect)rect {
@@ -166,7 +95,7 @@
             NSRange matchRange = [match range];
             CGRect start = [self rectForLetterAtIndex:matchRange.location];
             CGRect end = [self rectForLetterAtIndex:matchRange.location + matchRange.length - 1];
-            CGRect rect = CGRectMake(start.origin.x, start.origin.y, end.origin.x - start.origin.x + end.size.width + 6, start.size.height);
+            CGRect rect = CGRectMake(start.origin.x, start.origin.y, end.origin.x - start.origin.x + end.size.width + 5, start.size.height);
             
             if (CGRectContainsRect(self.bounds, rect)) {
                 CGContextBeginPath(context);
