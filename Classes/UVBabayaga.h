3d80b0d4fda22d830accdab556093556268ad9e5
Austin Taylor <austin.taylor@gmail.com>
2013-08-27 14:47:54 -0400

    WIP babayaga

---
+++ Classes/UVBabayaga.h
@@ -0,0 +1,13 @@
+//
+//  UVBabayaga.h
+//  UserVoice
+//
+//  Created by Austin Taylor on 8/27/13.
+//  Copyright (c) 2013 UserVoice Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UVBabayaga : NSObject
+
+@end
---
+++ Classes/UVBabayaga.m
@@ -0,0 +1,13 @@
+//
+//  UVBabayaga.m
+//  UserVoice
+//
+//  Created by Austin Taylor on 8/27/13.
+//  Copyright (c) 2013 UserVoice Inc. All rights reserved.
+//
+
+#import "UVBabayaga.h"
+
+@implementation UVBabayaga
+
+@end
