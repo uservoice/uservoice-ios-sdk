c880e206cc6b7b7b4b168f42e568d69c6c0095de
Austin Taylor <austin.taylor@gmail.com>
2013-09-19 14:48:59 -0400

    WIP deflections

---
+++ Classes/UVDeflection.h
@@ -0,0 +1,13 @@
+//
+//  UVDeflection.h
+//  UserVoice
+//
+//  Created by Austin Taylor on 9/19/13.
+//  Copyright (c) 2013 UserVoice Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UVDeflection : NSObject
+
+@end
